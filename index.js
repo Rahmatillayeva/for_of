d6d1c7943c4755907e7e40276645ec7000b950cd
Rahmatillayeva <shaxrizodamrahmatillayeva@gmail.com>
2024-02-28 15:13:22 +0500

    array

---
+++ index.js
@@ -0,0 +1,50 @@
+// masala #1
+
+// const numbers = [1, 3, 5, 9];
+
+// let Add = function (number) {
+//   let s = 0;
+//   for (let son of number) {
+//     s += son;
+//   }
+//   return s;
+// };
+// console.log(Add(numbers));
+
+// --------------------------------------------------------------
+
+// masala #2
+
+// const numbers = [3.02, -3.65, 5, -9];
+// const updateArray = [];
+// for (const number of numbers) {
+//   if (number > 0) {
+//     updateArray.push(Math.round(number));
+//   } else if (number < 0) {
+//     updateArray.push(Math.round(number) * -1);
+//   }
+// }
+// console.log(updateArray);
+
+// ---------------------------------------------------------------
+
+// masala #3
+
+// const summa = [275, 40, 430];
+// const paymentPrice = [];
+// const calcTip = function(number) {
+// for (const narx of number) {
+//   if (narx >= 50 && narx <= 300) {
+//     paymentPrice.push(narx * 0.15 + narx);
+//   } else if (narx < 50) {
+//     paymentPrice.push(narx);
+//   } else if (narx > 300) {
+//     paymentPrice.push(narx * 0.2 + narx);
+//   }
+// }
+// console.log(`Umumiy hisob: ${paymentPrice}`);
+// }
+// calcTip(summa);
+
+
+// --------------------------------------------------------------
